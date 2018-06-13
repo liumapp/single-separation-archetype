c020e413f203079fce91ce3b769aeeefed8adb4c
liumapp <liumapp.com@gmail.com>
2018-06-13 11:24:05 +0800

    rm unnecessary things and add cors config

--- archetype-service/src/main/java/com/liumapp/archetype/separation/config/Config.java
+++ archetype-service/src/main/java/com/liumapp/archetype/separation/config/Config.java
@@ -1,17 +1,22 @@
 package com.liumapp.archetype.separation.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 /**
  * Created by liumapp on 9/28/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
 @Configuration
-@ImportResource(locations = {"classpath:/spring/applicationContext*.xml"})
-public class Config {
+public class Config extends WebMvcConfigurerAdapter {
 
-
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
 
 }
